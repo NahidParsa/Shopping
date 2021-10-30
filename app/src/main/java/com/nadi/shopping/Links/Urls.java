e68a2804a4583df50a32df668cc2d517b2035c70
Nadi <parsapage@gmail.com>
2021-10-30 19:09:39 +0330

    FirstCommit

--- app/src/main/java/com/nadi/shopping/Links/Urls.java
+++ app/src/main/java/com/nadi/shopping/Links/Urls.java
@@ -2,7 +2,7 @@ package com.nadi.shopping.Links;
 
 public class Urls {
 
-    private static final String address = "192.168.1.2";
+    private static final String address = "192.168.1.3";
     public static final String BASE_URLS = "http://"+ address +"/shopping/";
 
     public static final String PRODUCT_AMAZING_VOLLEY = "http://"+address+"/shopping/getAmazingOfferProduct.php";
